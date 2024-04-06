4880242cd1e2f5c2446aac415385a3451c591201
infolion <jwsong5160@gmail.com>
2024-04-06 15:17:22 +0900

    first commit

---
+++ pages/posts/first-page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function FirstPage() {
+  return (
+    <>
+      <h1>Hi ! my name is Junwoo Song and I am a student in the Department of Information Systems at Hanyang University</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
